49e7eaefeda0148446ce36ffe0cf1d12965d712b
Joel Feinstein <jnfeinstein@gmail.com>
2021-03-14 12:16:46 -0400

    Spring Initializr

---
+++ src/test/kotlin/io/joel/bank/BankApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.joel.bank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
